a1830e3a07cc9f1f915cb70036e8d1ba054d8cc7
ilhammhrrm <90167055+ilhammhrrm@users.noreply.github.com>
2022-04-26 21:31:01 +0700

    Add files via upload

---
+++ Akhmad Ilham M_009_Insertion sort.cpp
@@ -0,0 +1,42 @@
+//insertion sort
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int y;
+    //Input pertama jumlah dalam array
+    //inisialisasi variabel
+    cout << "Masukan Jumlah array yang di inginkan : ";
+    cin >> y;
+    //array akan mengikuti jumlah array yang di inputkan
+    //array dinamis
+    int array[y];
+    //Input kedua isi array
+    //perintah untuk looping atau mengulang
+    for (int loop=0;loop<y;loop++){
+        cout << " Masukkan Isi array " << loop << " : ";
+        cin >> array[loop];
+        cout << endl;
+    }
+    //----proses sorting dimulai----
+    for(int loop=1; loop<y; loop++){
+        int key = array[loop];
+        int j = loop-1;
+        //jika nilai j lebih dari sama dengan 0 dan
+        //array j lebih besar dari key maka akan ditukar
+        while(j>=0 && array[j] > key){
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = key;
+    }
+    //-----Proses sorting berakhir-----
+    //Outout hasil sorting
+    cout << "hasil array yang sudah di sorting" << endl;
+        for (int m=0;m<y;m++){
+    cout << array[m] << " ";
+    }
+    return 0;
+}
